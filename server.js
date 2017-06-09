80fe5f3e7f295a909fe4e360f52db9ee84c868d1
System <none>
2017-06-09 06:41:59 +0000

    💴🚡mongoose won't connect.  saving before trying something else

--- server.js
+++ server.js
@@ -1,13 +1,39 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const axios = require('axios'); // for http requests
+const mongodb = require('mongodb');
+const mongodbUri = require('mongodb-uri'); // for converting standard mongoDB uri to mongoose-usable string uri
 const mongoose = require('mongoose'); // for talking to mongoDB
+const bodyParser = require('body-parser');
+// const axios = require('axios'); // for http requests
 
-//Standard URI format: mongodb://[dbuser:dbpassword@]host:port/dbname
 const uri = 'mongodb://'+process.env.USER+':'+process.env.PASS+'@'+process.env.HOST+':'+process.env.PORT+'/'+process.env.DB;
 
-mongoose.Promise = global.Promise;
-mongoose.connect(uri);
+// Reformat to a Mongoose connect string and connect() 
+const mongooseConnectString = mongodbUri.formatMongoose(uri);
+console.log(mongooseConnectString);
+// Mongoose by default sets the auto_reconnect option to true
+// add 30 second connection timeout to allow for enough time to connect
+
+const options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS:   30000 } }, 
+                replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };       
+
+// mongoose.Promise = global.Promise;
+mongoose.connect(mongooseConnectString, options);
+console.log(mongooseConnectString);
+
+var conn = mongoose.connection;             
+ 
+conn.on('error', console.error.bind(console, 'connection error:'));  
+ 
+conn.once('open', function() {
+  // Wait for the database connection to establish, then start the app.                         
+});
+
+// Test for connection success 
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Connection error: '));
+db.once('open', function callback () {
+    console.log('Successfully connected to MongoDB');
+});
 
 const app = express();
 app.use(bodyParser.json());

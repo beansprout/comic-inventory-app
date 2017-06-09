34605dab55e67cc4c83175d094bfa187568e58fe
System <none>
2017-06-09 07:15:55 +0000

    📖💫 Updated with Glitch

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
-const mongodb = require('mongodb');
+const mongodb = require('mongodb'); //database
 const mongoose = require('mongoose'); // for talking to mongoDB
 const bodyParser = require('body-parser');
 // const axios = require('axios'); // for http requests
+
+
+//--------database connection--------------------
+// connects to mlab 
+//question: leave here or put somewhere else?
 
 const mongodbUri = 'mongodb://'+process.env.USER+':'+process.env.PASS+'@'+process.env.HOST+':'+process.env.PORT+'/'+process.env.DB;
 
@@ -15,12 +20,14 @@ const options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS
 mongoose.Promise = global.Promise;
 mongoose.connect(mongodbUri, options);
 
+//-----------------------------------------------
+
 const app = express();
 app.use(bodyParser.json());
 
 app.get('/test', (req, res) => {
-  res.send("Hello World");
-  console.log('received get request');;
+  res.send(req);
+  console.log('received get request');
 })
 
 app.listen(8080, (req, res) => {

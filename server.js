cfe9c269305c0011d52f5e283f5ba6d32f0e95c6
beansprout <christinegierer@gmail.com>
2017-06-10 00:03:37 -0400

    refactored dbconnection added test routes for 3rd party api

--- server.js
+++ server.js
@@ -13,8 +13,15 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 
 //--------database connection--------------------
-const mongodbUri = 'mongodb://'+process.env.DB_USER+':'+process.env.PASS+'@'+process.env.HOST+':'+process.env.DB_PORT+'/'+process.env.DB;
+const dbUser = process.env.DB_USER;
+const pass = process.env.PASS;
+const host = process.env.HOST;
+const dbport = process.env.DB_PORT;
+const db = process.env.DB;
+
+const mongodbUri = `mongodb://${dbUser}:${pass}@${host}:${dbport}/${db}`;
 console.log(mongodbUri);
+
 // Mongoose by default sets the auto_reconnect option to true
 // add 30 second connection timeout to allow for enough time to connect
 const options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS:   30000 } }, 
@@ -22,7 +29,6 @@ const options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS
 
 mongoose.Promise = global.Promise;
 mongoose.connect(mongodbUri, options);
-
 //-----------------------------------------------
 
 const apiKey = process.env.API_KEY;
@@ -33,14 +39,43 @@ app.get('/', (req, res) => {
   console.log('received get request');
 });
 
-app.get('/test', (req, res) => {
-  const url = `${baseUrl}characters?api_key=${apiKey}&filter=name:${req.params.name}&format=json`;
+app.get('/volumes', (req, res) => {
+  const url = `${baseUrl}volumes?api_key=${apiKey}&filter=name:${req.params.name}&format=json`;
   axios.get(url).then((response) => {
     res.send(response.data);
   }).catch((err) => {
     res.send(err);
   });
 });
+
+app.get('/issues', (req, res) => {
+  const url = `${baseUrl}volumes?api_key=${apiKey}&filter=name:${req.params.name}&format=json`;
+  axios.get(url).then((response) => {
+    res.send(response.data);
+  }).catch((err) => {
+    res.send(err);
+  });
+});
+
+app.get('/publishers', (req, res) => {
+  const url = `${baseUrl}volumes?api_key=${apiKey}&filter=name:${req.params.name}&format=json`;
+  axios.get(url).then((response) => {
+    res.send(response.data);
+  }).catch((err) => {
+    res.send(err);
+  });
+});
+
+
+app.get('/search', (req, res) => {
+  const url = `${baseUrl}volumes?api_key=${apiKey}&filter=name:${req.params.name}&format=json`;
+  axios.get(url).then((response) => {
+    res.send(response.data);
+  }).catch((err) => {
+    res.send(err);
+  });
+});
+
 
 app.listen(port, (req, res) => {
   console.log(`server listening on port ${port}`);

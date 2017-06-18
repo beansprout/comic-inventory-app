bea0c7e9a4c17efede306c99d10a6c935d409721
Christine Gierer <christinegierer@gmail.com>
2017-06-18 00:57:44 -0400

    typo fixed

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 8080;
 
 const app = express();
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 
